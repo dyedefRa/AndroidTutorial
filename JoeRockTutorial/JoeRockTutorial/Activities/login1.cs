4922fc5a2bb405792a9d72c0375ca84191c2ff3a
Baki OZTURK <22.exe.94@gmail.com>
2021-08-27 20:33:05 +0300

    gits

--- JoeRockTutorial/JoeRockTutorial/Activities/login1.cs
+++ JoeRockTutorial/JoeRockTutorial/Activities/login1.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace JoeRockTutorial.Activities
 {
@@ -16,6 +17,7 @@ namespace JoeRockTutorial.Activities
     public class login1 : Activity
     {
         private Button btnSignUp;
+        private ProgressBar progressBar;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,14 +27,29 @@ namespace JoeRockTutorial.Activities
             // Create your application here
 
             btnSignUp = FindViewById<Button>(Resource.Id.btnSignUpEmail);
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
             btnSignUp.Click += (s, e) =>
             {
                 FragmentTransaction transaction = FragmentManager.BeginTransaction();
                 dialog_SignIn signUpDialog = new dialog_SignIn();
-                signUpDialog.Show(transaction,"dialog_fragment");
+                signUpDialog.Show(transaction, "dialog_fragment");
+
+                signUpDialog.mOnSignUpComplete += SignUpDialog_mOnSignUpComplete;
             };
         }
 
-    
+        private void SignUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e)
+        {
+            progressBar.Visibility = ViewStates.Visible;
+            Thread thread = new Thread(ActLikeARequest);
+            thread.Start();
+           
+
+        }
+        private void ActLikeARequest()
+        {
+            Thread.Sleep(3000);
+            RunOnUiThread(() => { progressBar.Visibility = ViewStates.Invisible; });
+        }
     }
 }
--- JoeRockTutorial/JoeRockTutorial/Models/dialog_SignIn.cs
+++ JoeRockTutorial/JoeRockTutorial/Models/dialog_SignIn.cs
@@ -9,18 +9,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace JoeRockTutorial.Models
 {
     public class dialog_SignIn : DialogFragment
     {
+        private EditText txtFirstName;
+        private EditText txtEmail;
+        private EditText txtPassword;
+        private Button btnSignUp;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
+            //Bu classtan findViewById methodu cekecegız.
+
             var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+
+            txtFirstName = view.FindViewById<EditText>(Resource.Id.txtFirstName);
+            txtEmail = view.FindViewById<EditText>(Resource.Id.txtEmailAddress);
+            txtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+            btnSignUp = view.FindViewById<Button>(Resource.Id.btnDialogEmail);
+
+            btnSignUp.Click += BtnSignUp_Click;
 
             return view;
         }
+
+        private void BtnSignUp_Click(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            mOnSignUpComplete.Invoke(this, new OnSignUpEventArgs(txtFirstName.Text, txtEmail.Text, txtPassword.Text));
+            this.Dismiss();
+        }
+
+        public event EventHandler<OnSignUpEventArgs> mOnSignUpComplete;
+
 
         //public override void OnActivityCreated(Bundle savedInstanceState)
         //{
@@ -28,5 +53,36 @@ namespace JoeRockTutorial.Models
         //    base.OnActivityCreated(savedInstanceState);
         //    //Dialog.Window.Attributes.WindowAnimations=Resource.Style.dialog_ani
         //}
+    }
+
+    public class OnSignUpEventArgs : EventArgs
+    {
+        public OnSignUpEventArgs(string firstName, string email, string password) : base()
+        {
+            FirstName = firstName;
+            Email = email;
+            Password = password;
+        }
+        private string mFirstName;
+        private string mEmail;
+        private string mPassword;
+
+        public string FirstName
+        {
+            get { return mFirstName; }
+            set { mFirstName = value; }
+        }
+
+        public string Email
+        {
+            get { return mEmail; }
+            set { mEmail = value; }
+        }
+
+        public string Password
+        {
+            get { return mPassword; }
+            set { mPassword = value; }
+        }
     }
 }
