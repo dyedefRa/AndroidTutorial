6554c30377e3c91ca4e514d57a3e4e1e6cc7b675
Baki OZTURK <22.exe.94@gmail.com>
2021-09-26 19:47:25 +0300

    gitS

--- WhatsApp/WhatsApp/Activities/PhoneLoginActivity.cs
+++ WhatsApp/WhatsApp/Activities/PhoneLoginActivity.cs
@@ -1,13 +1,17 @@
 ﻿using Android.App;
 using Android.Content;
+
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Firebase;
+using Firebase.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using WhatsApp.Helper;
 using static Android.Views.View;
 
 namespace WhatsApp.Activities
@@ -17,6 +21,9 @@ namespace WhatsApp.Activities
     {
         private Button sendVerificiationCodeButton, verifyButton;
         private EditText inputPhoneNumber, inputVerificationCode;
+
+
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,12 +38,99 @@ namespace WhatsApp.Activities
             sendVerificiationCodeButton.SetOnClickListener(this); // => OnClick
         }
 
+        private string mVerificationId;
+        private PhoneAuthProvider.ForceResendingToken mResendToken;
+
         public void OnClick(View v)
         {
-            sendVerificiationCodeButton.Visibility = ViewStates.Invisible;
-            inputPhoneNumber.Visibility = ViewStates.Invisible;
-            verifyButton.Visibility = ViewStates.Visible;
-            inputVerificationCode.Visibility = ViewStates.Visible;
+            //sendVerificiationCodeButton.Visibility = ViewStates.Invisible;
+            //inputPhoneNumber.Visibility = ViewStates.Invisible;
+            //verifyButton.Visibility = ViewStates.Visible;
+            //inputVerificationCode.Visibility = ViewStates.Visible;
+
+            string phoneNumber = inputPhoneNumber.Text;
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                Toast.MakeText(this, "Phone number is required", ToastLength.Short)
+                    .Show();
+            }
+            else
+            {
+
+
+
+                var timeout = new Java.Lang.Long(60L);
+                PhoneAuthOptions options = PhoneAuthOptions.NewBuilder()
+                    .SetPhoneNumber(phoneNumber)
+                    .SetTimeout(timeout, Java.Util.Concurrent.TimeUnit.Seconds)
+                    .SetActivity(this)
+                    .SetCallbacks(new PhoneAuthCallbacks())
+                    .Build();
+
+                PhoneAuthProvider.VerifyPhoneNumber(options);
+
+                //PhoneAuthProvider.GetInstance(FirebaseClient.GetFirebaseAuth())
+                //    .VerifyPhoneNumber(
+                //    phoneNumber,
+                //    60,
+                //    Java.Util.Concurrent.TimeUnit.Seconds,
+                //    this,
+                //    callbacks)
+
+            }
+
+
         }
+
+
+
+        private void signInWithPhoneAuthCredential(PhoneAuthCredential credential)
+        {
+
+            TaskCompletionListener taskCompletionListener = new TaskCompletionListener();
+            taskCompletionListener.Complete += TaskCompletionListener_Complete;
+
+            FirebaseClient.GetFirebaseAuth().SignInWithCredential(credential)
+                .AddOnCompleteListener(taskCompletionListener);
+
+
+        }
+
+        private void TaskCompletionListener_Complete(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //public class PhoneAuthCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks, View.IOnClickListener
+        //{
+
+        //    public override void OnVerificationCompleted(PhoneAuthCredential credential)
+        //    {
+        //        signInWithPhoneAuthCredential(credential);
+        //    }
+
+        //    public override void OnCodeSent(string verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken)
+        //    {
+        //        // The SMS verification code has been sent to the provided phone number, we
+        //        // now need to ask the user to enter the code and then construct a credential
+        //        // by combining the code with a verification ID.
+        //        //base.OnCodeSent(verificationId, forceResendingToken);
+
+        //    }
+
+        //    public override void OnVerificationFailed(FirebaseException p0)
+        //    {
+        //        Toast.MakeText(this, "Invalid Phone Number , Please enter correct phone number with yout country code", ToastLength.Short)
+        //          .Show();
+        //    }
+
+        //    public void OnClick(View v)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
     }
 }
+
+
---
+++ WhatsApp/WhatsApp/Helper/PhoneAuthCallbacks.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase;
+using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WhatsApp.Activities;
+
+namespace WhatsApp.Helper
+{
+    public class PhoneAuthCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    {
+
+        private string mVerificationId;
+        private PhoneAuthProvider.ForceResendingToken mResendToken;
+
+        public override void OnVerificationCompleted(PhoneAuthCredential p0)
+        {
+            signInWithPhoneAuthCredential(p0);
+        }
+
+        public override void OnVerificationFailed(FirebaseException p0)
+        {
+            Toast.MakeText(new PhoneLoginActivity(), "Invalid Phone Number , Please enter correct phone number with yout country code", ToastLength.Short)
+                 .Show();
+        }
+        public override void OnCodeSent(string p0, PhoneAuthProvider.ForceResendingToken p1)
+        {
+            mVerificationId = p0;
+            mResendToken = p1;
+
+            Toast.MakeText(new PhoneLoginActivity(), "Code has been sent", ToastLength.Short)
+               .Show();
+
+            base.OnCodeSent(p0, p1);
+        }
+
+
+
+
+
+     
+    }
+}
